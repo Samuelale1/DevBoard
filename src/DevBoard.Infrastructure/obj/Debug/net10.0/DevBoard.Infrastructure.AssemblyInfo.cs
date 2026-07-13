1a8ec1e239a11d825ade412988b50768f3b15449
Samuelale1 <alesamuelayomide@gmail.com>
2026-07-13 19:14:31 +0100

    Ran dotnet build to ensure no compile or runtime errors

--- src/DevBoard.Infrastructure/obj/Debug/net10.0/DevBoard.Infrastructure.AssemblyInfo.cs
+++ src/DevBoard.Infrastructure/obj/Debug/net10.0/DevBoard.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevBoard.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4162694e3d5d471bf139552ec8553f4ca0f840e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevBoard.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevBoard.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
